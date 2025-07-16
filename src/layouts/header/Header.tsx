9424c35b13f1beaf215ad4639a278b4f51818b12
AlonaRahilevych <ragilevic@gmail.com>
2025-07-16 22:04:10 +0200

    feat: create header  compoment

---
+++ src/layouts/header/Header.tsx
@@ -0,0 +1,26 @@
+import { Button } from "../../components/ui/Button/Button";
+import { useWindowSize } from "../../hooks/useWindowSize";
+import styles from "./Header.module.css";
+import { MobileMenu } from "./MobileMenu";
+export const Header = () => {
+  const { width } = useWindowSize();
+  const isMobile = width < 768;
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <span>Event</span>
+          <span>Rave</span>
+        </div>
+        {isMobile ? (
+          <MobileMenu />
+        ) : (
+          <div className={styles.buttons}>
+            <Button className={styles.login}>Log in</Button>
+            <Button> Sign up</Button>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
