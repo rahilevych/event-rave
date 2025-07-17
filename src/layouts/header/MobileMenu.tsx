8c19d3baa7af2b9b20a4268aa84dd0ff06469483
AlonaRahilevych <ragilevic@gmail.com>
2025-07-17 21:17:03 +0200

    feat:create MObileMenu component

---
+++ src/layouts/header/MobileMenu.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { MobileDropdown } from "./MobileDropdown";
+import { MdClose, MdPerson } from "react-icons/md";
+import styles from "./MobileMenu.module.css";
+import { AnimatePresence, motion } from "motion/react";
+
+export const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={styles.menu}>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className={`${styles.dropdown} ${styles.opened}`}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.25 }}
+          >
+            <MobileDropdown />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {!isOpen ? (
+        <MdPerson onClick={() => setIsOpen(true)} className={styles.openIcon} />
+      ) : (
+        <MdClose
+          className={styles.closeIcon}
+          onClick={() => setIsOpen(false)}
+        />
+      )}
+    </div>
+  );
+};
