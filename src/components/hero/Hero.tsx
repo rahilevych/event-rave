1fa8d50887acbecde01de2bd2c8bc8429032bf32
AlonaRahilevych <ragilevic@gmail.com>
2025-07-22 13:25:52 +0200

    feat: create hero section

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,12 @@
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={`container ${styles.hero}`}>
+      <div className={styles.img}>
+        <img src="/src/assets/images/hero.png" alt="" />
+      </div>
+      <h1>Made for those who do</h1>
+    </section>
+  );
+};
