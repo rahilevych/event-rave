738f03ecff7039746042474eff2aa62854fb79de
AlonaRahilevych <ragilevic@gmail.com>
2025-07-22 13:58:44 +0200

    test: add tests for hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
   return (
     <section className={`container ${styles.hero}`}>
       <div className={styles.img}>
-        <img src="/src/assets/images/hero.png" alt="" />
+        <img src="/src/assets/images/hero.png" alt="hero" />
       </div>
       <h1>Made for those who do</h1>
     </section>
