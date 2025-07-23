26930a067c04078373a5851e1c6c66554a8ae26f
AlonaRahilevych <ragilevic@gmail.com>
2025-07-23 17:17:00 +0200

    chore: update dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,18 @@
-import { createDefaultPreset } from "ts-jest";
-
 export default {
+  preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/jest.setup.ts"],
-  transform: {
-    ...createDefaultPreset().transform,
-  },
-
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   moduleNameMapper: {
     "\\.module\\.css$": "identity-obj-proxy",
     "\\.css$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.app.json",
+      },
+    ],
   },
 };
