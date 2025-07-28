3abe83a78e7ab0dcdba47535659917d9e0638e0b
AlonaRahilevych <ragilevic@gmail.com>
2025-07-28 16:13:31 +0200

    feat: create EventCard component

---
+++ src/components/events-section/EventCard.tsx
@@ -0,0 +1,20 @@
+import styles from './EventCard.module.css';
+import { Event } from '../../types/types';
+
+type EventCardProps = {
+  card: Event;
+};
+export const EventCard = ({ card }: EventCardProps) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.img}>
+        <img src={card.image} alt="" />
+      </div>
+      <div className={styles.info}>
+        <p className={styles.title}>{card.title}</p>
+        <p className={styles.date}>{card.date}</p>
+        <p className={styles.location}>{card.location}</p>
+      </div>
+    </div>
+  );
+};
