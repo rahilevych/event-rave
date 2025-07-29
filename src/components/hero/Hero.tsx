3ea6db51dc7a2940d9e19646c83e5139289f167d
AlonaRahilevych <ragilevic@gmail.com>
2025-07-29 16:05:05 +0200

    fix:img url

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
     <section className={`container ${styles.hero}`}>
       <div className={styles.img}>
         <img
-          src="https://unsplash.com/de/fotos/gruppe-von-leuten-die-konzert-beobachten-U7HLzMO4SIY"
+          src="https://images.unsplash.com/photo-1522158637959-30385a09e0da?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="hero"
         />
       </div>
