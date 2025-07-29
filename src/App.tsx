bb4507d14d5e53a1781053b206bccf986605c4b5
AlonaRahilevych <ragilevic@gmail.com>
2025-07-29 15:18:42 +0200

    feat: create HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css';
+import "./App.css";
+import "../src/styles/global.css";
+import { HomePage } from "./pages/home/HomePage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
 
 export default App;
