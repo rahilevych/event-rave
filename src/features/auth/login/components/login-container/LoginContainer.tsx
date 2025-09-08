960ca9dcdbaa4312849c6e87946650eb0309e083
AlonaRahilevych <ragilevic@gmail.com>
2025-09-08 17:32:41 +0200

    style: make login form responsive

--- src/features/auth/login/components/login-container/LoginContainer.tsx
+++ src/features/auth/login/components/login-container/LoginContainer.tsx
@@ -8,11 +8,20 @@ export const LoginContainer = () => {
       <header>
         <Logo />
       </header>
-      <main>
-        <div className={styles.container}>
-          <h2>Sign in</h2>
-          <p>Welcome back! Please enter your details</p>
-          <LoginForm />
+      <main className={styles.main}>
+        <div className={styles.content}>
+          {' '}
+          <div className={styles.container}>
+            <h2>Sign in</h2>
+            <p>Welcome back! Please enter your details</p>
+            <LoginForm />
+          </div>
+          <div className={styles.img}>
+            <img
+              src="https://images.unsplash.com/photo-1558008258-3256797b43f3?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
         </div>
       </main>
     </>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,9 @@
 import { LoginContainer } from '../features/auth/login/components/login-container/LoginContainer';
-
+import styles from '../features/auth/login/styles/LoginPage.module.css';
 export const LoginPage = () => {
   return (
     <div className="container">
-      <main>
+      <main className={styles.content}>
         <LoginContainer />
       </main>
     </div>
