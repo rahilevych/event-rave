22737c13af1a10c62828ce383ebfde762b33a9db
AlonaRahilevych <ragilevic@gmail.com>
2025-09-22 21:57:14 +0200

    refactor:update Event section

--- src/features/home/components/events-section/EventsSection.tsx
+++ src/features/home/components/events-section/EventsSection.tsx
@@ -1,51 +1,16 @@
-import { useState } from 'react';
-
 import styles from './EventSection.module.css';
 
-import { category, days, events, types } from '../data/events';
+import { events } from '../data/events';
 import { Title } from '../../../../shared/ui/title/Title';
-import { Dropdown } from '../../../../shared/ui/dropdown/Dropdown';
 import { EventCard } from './EventCard';
 
 export const EventsSection = () => {
   const title = 'Upcoming events';
-  const [searchParams, setSearchParams] = useState({
-    day: 'Weekdays',
-    type: 'Event type',
-    category: 'Any category',
-  });
-
-  const handleDropdownChange = (key: string, value: string) => {
-    setSearchParams((prev) => ({
-      ...prev,
-      [key]: value,
-    }));
-  };
 
   return (
     <div className={`container ${styles.events}`}>
       <header className={styles.header}>
         <Title text={title} />
-        <div className={styles.dropdowns}>
-          <Dropdown
-            className={styles.dropdown}
-            label={searchParams.type}
-            items={types}
-            onSelect={(value) => handleDropdownChange('type', value)}
-          />
-          <Dropdown
-            className={styles.dropdown}
-            label={searchParams.day}
-            items={days}
-            onSelect={(value) => handleDropdownChange('type', value)}
-          />
-          <Dropdown
-            className={styles.dropdown}
-            label={searchParams.category}
-            items={category}
-            onSelect={(value) => handleDropdownChange('type', value)}
-          />
-        </div>
       </header>
       <main>
         {events === null ? (
