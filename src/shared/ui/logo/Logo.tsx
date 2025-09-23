622d4c32bcf01cd06cb6b8943b7fffd8e0f1efcd
AlonaRahilevych <ragilevic@gmail.com>
2025-09-23 12:38:25 +0200

    refactor:update logo component

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -1,14 +1,17 @@
+import { splitText } from '../../utils/splitText';
 import styles from './Logo.module.css';
 import { Link } from 'react-router-dom';
-
-export const Logo = () => {
+type LogoProps = {
+  text: string;
+};
+export const Logo = ({ text }: LogoProps) => {
+  const { beginning, lastWord } = splitText(text);
   return (
     <Link to="/">
-      {' '}
-      <div data-testid="logo" className={styles.logo}>
-        <span>Event</span>
-        <span>Rave</span>
-      </div>{' '}
+      <div className={`${styles.logo}`}>
+        {beginning && <>{beginning}</>}
+        <span className={styles.accent}>{lastWord}</span>
+      </div>
     </Link>
   );
 };
