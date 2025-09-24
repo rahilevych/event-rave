c10160e5636ac893fa8580d25ffd24df49520045
AlonaRahilevych <ragilevic@gmail.com>
2025-09-24 16:15:44 +0200

    refactor:make components more reusable

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -3,12 +3,13 @@ import styles from './Logo.module.css';
 import { Link } from 'react-router-dom';
 type LogoProps = {
   text: string;
+  className?: string;
 };
-export const Logo = ({ text }: LogoProps) => {
+export const Logo = ({ text, className }: LogoProps) => {
   const { beginning, lastWord } = splitText(text);
   return (
     <Link to="/">
-      <div className={`${styles.logo}`}>
+      <div className={`${styles.logo} ${className}`}>
         {beginning && <>{beginning}</>}
         <span className={styles.accent}>{lastWord}</span>
       </div>
