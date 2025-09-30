37080a15c574d9d16b0202db6812918dd647e292
AlonaRahilevych <ragilevic@gmail.com>
2025-09-30 13:02:49 +0200

    test: update tests for login form

--- src/features/auth/__tests__/LoginForm.test.tsx
+++ src/features/auth/__tests__/LoginForm.test.tsx
@@ -1,32 +1,46 @@
-// import { render, screen } from '@testing-library/react';
-// import { LoginForm } from '../components/LoginForm';
-// import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { LoginForm } from '../components/form/LoginForm';
+import { MemoryRouter } from 'react-router-dom';
 
-// describe('LoginForm component', () => {
-//   const user = userEvent.setup();
-//   test('should render from with correct inputs', () => {
-//     render(<LoginForm />);
-//     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
-//     expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
-//   });
-//   test('user can type in both inputs', async () => {
-//     render(<LoginForm />);
-//     const emailInput = screen.getByPlaceholderText(
-//       /email/i,
-//     ) as HTMLInputElement;
-//     const passwordInput = screen.getByPlaceholderText(
-//       /password/i,
-//     ) as HTMLInputElement;
+describe('LoginForm component', () => {
+  const user = userEvent.setup();
+  test('should render from with correct inputs', () => {
+    render(
+      <MemoryRouter>
+        <LoginForm />
+      </MemoryRouter>,
+    );
 
-//     await user.type(emailInput, 'user@gmail.com');
-//     await user.type(passwordInput, 'passsword');
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
+  });
+  test('user can type in both inputs', async () => {
+    render(
+      <MemoryRouter>
+        <LoginForm />
+      </MemoryRouter>,
+    );
+    const emailInput = screen.getByPlaceholderText(
+      /email/i,
+    ) as HTMLInputElement;
+    const passwordInput = screen.getByPlaceholderText(
+      /password/i,
+    ) as HTMLInputElement;
 
-//     expect(emailInput.value).toBe('user@gmail.com');
-//     expect(passwordInput.value).toBe('passsword');
-//   });
-//   test('render 2 buttons', () => {
-//     render(<LoginForm />);
-//     const buttons = screen.getAllByRole('button');
-//     expect(buttons).toHaveLength(3);
-//   });
-// });
+    await user.type(emailInput, 'user@gmail.com');
+    await user.type(passwordInput, 'passsword');
+
+    expect(emailInput.value).toBe('user@gmail.com');
+    expect(passwordInput.value).toBe('passsword');
+  });
+  test('render 2 buttons', () => {
+    render(
+      <MemoryRouter>
+        <LoginForm />
+      </MemoryRouter>,
+    );
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+  });
+});
