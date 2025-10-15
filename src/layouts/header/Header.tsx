0b8e73cbdef923819dd9c3a946fedc22d76c8402
Alona Rahilevych <alona.rahilevych@gmail.com>
2025-10-15 11:39:12 +0200

    refactor:update logic for logged in / logged out user

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -6,27 +6,42 @@ import { MobileMenu } from './MobileMenu';
 
 import { Logo } from '../../shared/ui/logo/Logo';
 import { SearchSection } from '../../features/search/components/search-section/SearchSection';
+import { useAuthStore } from '../../features/auth/model/AuthStore';
+import { LoggedInUserDropdown } from '../../shared/ui/logged-in-user-dropdown/LoggedInUserDropdown';
 export const Header = () => {
   const { width } = useWindowSize();
   const isMobile = width < 768;
   const text = 'Event Rave';
+  const isAuth = useAuthStore((state) => state.isAuth);
+
   return (
     <header className={styles.header}>
       <div className={styles.content}>
         <Logo text={text} className={styles.logo} />
         {isMobile ? (
-          <MobileMenu />
+          isAuth ? (
+            <LoggedInUserDropdown />
+          ) : (
+            <MobileMenu />
+          )
         ) : (
           <>
             <SearchSection />
-            <div className={styles.buttons}>
-              <Link to="login">
-                <Button className={styles.login}>Log in</Button>
-              </Link>
-              <Link to="registration">
-                <Button className={styles.signup}> Sign up</Button>
-              </Link>
-            </div>
+
+            {isAuth ? (
+              <div className={styles.buttons}>
+                <LoggedInUserDropdown />
+              </div>
+            ) : (
+              <div className={styles.buttons}>
+                <Link to="login">
+                  <Button className={styles.login}>Log in</Button>
+                </Link>
+                <Link to="registration">
+                  <Button className={styles.signup}> Sign up</Button>
+                </Link>
+              </div>
+            )}
           </>
         )}
       </div>
