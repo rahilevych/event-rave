0ff0dc1f98b1d61d70972532aede9e2a29a4bbba
Alona Rahilevych <alona.rahilevych@gmail.com>
2025-10-16 23:21:14 +0200

    feat update dropdown wrapper

--- src/shared/ui/dropdown/DropdownWrapper.tsx
+++ src/shared/ui/dropdown/DropdownWrapper.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import styles from './Dropdown.module.css';
+import styles from './DropdownWrapper.module.css';
 
 import { AnimatePresence, motion } from 'motion/react';
 import { useClickOutside } from '../../hooks/useClickOutside';
