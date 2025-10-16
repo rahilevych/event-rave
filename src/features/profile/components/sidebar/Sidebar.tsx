daf44ed655299ff0f70793227f0c6da5e97ab3ce
Alona Rahilevych <alona.rahilevych@gmail.com>
2025-10-16 23:08:36 +0200

    feat:create sidebar component

---
+++ src/features/profile/components/sidebar/Sidebar.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import styles from './Sidebar.module.css';
+import { SideBarItem } from '../../model/types';
+type SidebarProps = {
+  list: SideBarItem[];
+  onClick: (node: React.ReactNode) => void;
+  onTitleChange: (title: string) => void;
+};
+
+export const Sidebar = ({ list, onClick, onTitleChange }: SidebarProps) => {
+  const [activeTab, setActiveTab] = useState(list[0].id);
+
+  const handleClick = (item: SideBarItem) => {
+    onClick(item.component);
+    onTitleChange(item.label);
+    setActiveTab(item.id);
+  };
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.title}>Profile</div>
+      <ul className={styles.list}>
+        {list.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => handleClick(item)}
+            className={`${styles.listItem} ${
+              item.id === activeTab ? styles.active : ''
+            }`}
+          >
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
