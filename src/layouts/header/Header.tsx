3bb1c95d6a3a7cf7e1a8552adff499152d26af43
Alona Rahilevych <alona.rahilevych@gmail.com>
2025-10-18 11:58:38 +0200

    refactor: update headet nav

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,48 +1,29 @@
-import { Link } from 'react-router-dom';
 import { useWindowSize } from '../../shared/hooks/useWindowSize';
-import { Button } from '../../shared/ui/Button/Button';
 import styles from './Header.module.css';
 import { MobileMenu } from './MobileMenu';
-
 import { Logo } from '../../shared/ui/logo/Logo';
 import { SearchSection } from '../../features/search/components/search-section/SearchSection';
 import { useAuthStore } from '../../features/auth/model/AuthStore';
-import { LoggedInUserDropdown } from '../../shared/ui/logged-in-user-dropdown/LoggedInUserDropdown';
+
+import { NavDropdown } from './ui/nav-dropdown/NavDropdown';
+import { getMenuItems } from '../../config/menu-navigation';
 export const Header = () => {
   const { width } = useWindowSize();
   const isMobile = width < 768;
   const text = 'Event Rave';
   const isAuth = useAuthStore((state) => state.isAuth);
-
+  const menuItems = getMenuItems(isAuth);
   return (
     <header className={styles.header}>
       <div className={styles.content}>
         <Logo text={text} className={styles.logo} />
         {isMobile ? (
-          isAuth ? (
-            <LoggedInUserDropdown />
-          ) : (
-            <MobileMenu />
-          )
+          <MobileMenu />
         ) : (
-          <>
+          <div className={styles['laptop-menu']}>
             <SearchSection />
-
-            {isAuth ? (
-              <div className={styles.buttons}>
-                <LoggedInUserDropdown />
-              </div>
-            ) : (
-              <div className={styles.buttons}>
-                <Link to="login">
-                  <Button className={styles.login}>Log in</Button>
-                </Link>
-                <Link to="registration">
-                  <Button className={styles.signup}> Sign up</Button>
-                </Link>
-              </div>
-            )}
-          </>
+            <NavDropdown menuItems={menuItems} />
+          </div>
         )}
       </div>
     </header>
