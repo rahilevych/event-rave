534a31028f616d979e01dd26a7935e5f9d7340b9
Alona Rahilevych <alona.rahilevych@gmail.com>
2025-10-18 12:22:47 +0200

    refactor:update mobile menu

---
+++ src/features/navigation/components/mobile-menu/MobileMenu.tsx
@@ -0,0 +1,33 @@
+import styles from './MobileMenu.module.css';
+import { MdSearch } from 'react-icons/md';
+import { useState } from 'react';
+import MobileSearch from '../../../search/components/mobile/MobileSearch';
+
+import { useAuthStore } from '../../../auth/model/AuthStore';
+import { getMenuItems } from '../../../../config/menu-navigation';
+import { NavDropdown } from '../../ui/nav-dropdown/NavDropdown';
+
+export const MobileMenu = () => {
+  const [isSearchOpened, setIsSearchOpened] = useState(false);
+  const isAuth = useAuthStore((state) => state.isAuth);
+  const menuItems = getMenuItems(isAuth);
+
+  return (
+    <div data-testid="mobile-menu" className={styles.menu}>
+      {isSearchOpened && (
+        <MobileSearch
+          isSearchOpened={isSearchOpened}
+          setIsSearchOpened={setIsSearchOpened}
+        />
+      )}
+      <button
+        onClick={() => {
+          setIsSearchOpened(true);
+        }}
+      >
+        <MdSearch className={styles.icon} />
+      </button>{' '}
+      <NavDropdown menuItems={menuItems} />
+    </div>
+  );
+};
