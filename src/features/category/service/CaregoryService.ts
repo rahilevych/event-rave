3a9934dc3818d8a20b0d1e5c79c171aa89521d4a
Alona Rahilevych <ragilevic@gmail.com>
2025-10-20 17:11:15 +0200

    feat:create service with routes for category

---
+++ src/features/category/service/CaregoryService.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from 'axios';
+import api from '../../../shared/api/axiosInstance';
+import { Category } from '../model/types';
+
+export default class CategoryService {
+  static async getCategory(id: number): Promise<AxiosResponse> {
+    return api.get(`/categories/${id}`);
+  }
+  static async getCategories(): Promise<AxiosResponse> {
+    return api.get(`/categories`);
+  }
+  static async updateCategory(
+    id: number,
+    category: Partial<Category>,
+  ): Promise<AxiosResponse> {
+    return api.patch(`/categories/${id}`, category);
+  }
+  static async deleteCategory(id: number): Promise<AxiosResponse> {
+    return api.delete(`/categories/${id}`);
+  }
+  static async createCategory(
+    id: number,
+    category: Category,
+  ): Promise<AxiosResponse> {
+    return api.post(`/categories/${id}`, { category });
+  }
+}
