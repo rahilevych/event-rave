fe09db2d47bebdb365ba3957fe5a2492f9c8c18a
Alona Rahilevych <ragilevic@gmail.com>
2025-10-21 13:10:57 +0200

    feat:display events from db

--- src/features/event/ui/card/EventCard.tsx
+++ src/features/event/ui/card/EventCard.tsx
@@ -1,20 +1,24 @@
-import { Event } from '../../../../shared/types/types';
+import { converDate } from '../../../../shared/lib/convertDate';
+import { EventType } from '../../model/types';
 import styles from './EventCard.module.css';
 
 type EventCardProps = {
-  card: Event;
+  card: EventType;
 };
 export const EventCard = ({ card }: EventCardProps) => {
   return (
     <div className={styles.card} data-testid="event-card">
       <div className={styles.img}>
-        <img src={card.image} alt="event" />
+        <img src={card.imageUrl} alt="event" />
       </div>
       <div className={styles.info}>
         <p className={styles.title}>{card.title}</p>
-        <p className={styles.date}>{card.date}</p>
-        <p className={styles.location}>{card.location}</p>
-        <p className={styles.price}>{card.price}</p>
+        <p className={styles.date}>{converDate(card.date)}</p>
+        <p className={styles.venue}>
+          {card.venue}, {card.city}
+        </p>
+        <p className={styles.price}>{Math.floor(card.price)} $</p>
+        <p className={styles.organizer}>{card.organizer}</p>
       </div>
     </div>
   );
