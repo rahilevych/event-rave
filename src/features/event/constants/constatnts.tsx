879d4797de9d78b67a367ab9c4c829da2b943af3
Alona Rahilevych <ragilevic@gmail.com>
2025-11-02 22:21:08 +0100

    feat: create constatnts for filter section

---
+++ src/features/event/constants/constatnts.tsx
@@ -0,0 +1,9 @@
+import { MdEditCalendar } from 'react-icons/md';
+
+export const filters = [
+  { key: 'calendar', label: 'Date', icon: <MdEditCalendar /> },
+  { key: 'today', label: 'Today' },
+  { key: 'tomorrow', label: 'Tomorrow' },
+  { key: 'week', label: 'This week' },
+  { key: 'month', label: 'This month' },
+];
