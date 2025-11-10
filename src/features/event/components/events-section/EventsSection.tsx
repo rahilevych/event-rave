c869d1a1dbe1b74490d3ddc4a64996745572179d
Alona Rahilevych <ragilevic@gmail.com>
2025-11-10 12:52:59 +0100

    feat: add skeletons to pages and components

--- src/features/event/components/events-section/EventsSection.tsx
+++ src/features/event/components/events-section/EventsSection.tsx
@@ -2,9 +2,9 @@ import styles from './EventSection.module.css';
 import { Title } from '../../../../shared/ui/title/Title';
 import { EventSlider } from '../../ui/slider/EventSlider';
 import { useEventStore } from '../../model/EventStore';
-import { useFetch } from '../../../../shared/hooks/useFetch';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { EventSectionSkeleton } from './EventSectionSkeleton';
 
 interface EventSectionProps {
   categoryId: number;
@@ -13,7 +13,8 @@ interface EventSectionProps {
 
 export const EventsSection = ({ title, categoryId }: EventSectionProps) => {
   const getEvents = useEventStore((state) => state.getEvents);
-  const { data: events, execute } = useFetch(getEvents);
+  const eventsByCategory = useEventStore((state) => state.eventsByCategory);
+  const events = eventsByCategory[categoryId] || [];
   const navigate = useNavigate();
 
   const handleSeeAllClick = () => {
@@ -21,22 +22,20 @@ export const EventsSection = ({ title, categoryId }: EventSectionProps) => {
   };
 
   useEffect(() => {
-    execute({ categoryId: categoryId });
-  }, [categoryId, execute]);
+    getEvents({ categoryId: categoryId });
+  }, []);
 
-  return (
-    events && (
-      <section className={`container ${styles.events}`}>
-        <header className={styles.header}>
-          <Title text={title} className={styles.title} />
-          <p onClick={handleSeeAllClick}>see all</p>
-        </header>
-
-        <div className={styles.slider}>
-          {' '}
-          <EventSlider events={events} />
-        </div>
-      </section>
-    )
+  return events.length > 0 ? (
+    <section className={`container ${styles.events}`}>
+      <header className={styles.header}>
+        <Title text={title} className={styles.title} />
+        <p onClick={handleSeeAllClick}>see all</p>
+      </header>
+      <div className={styles.slider}>
+        <EventSlider events={events} />
+      </div>
+    </section>
+  ) : (
+    <EventSectionSkeleton />
   );
 };
