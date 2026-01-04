541117e8e9dd692fbb75905dd0f273dcdf49e51e
Alona Rahilevych <ragilevic@gmail.com>
2026-01-04 14:07:37 +0100

    refactor:  fix bug with favourites in mobile

--- src/features/navigation/components/mobile-menu/MobileMenu.tsx
+++ src/features/navigation/components/mobile-menu/MobileMenu.tsx
@@ -6,13 +6,16 @@ import MobileSearch from '../../../search/components/mobile/MobileSearch';
 import { useAuthStore } from '../../../auth/model/AuthStore';
 import { getMenuItems } from '../../../../config/menu-navigation';
 import { NavDropdown } from '../../ui/nav-dropdown/NavDropdown';
-import { useAuth } from '../../../auth/hooks/useAuth';
+
 import { HeartBtn } from '../../../../shared/ui/heart-btn/HeartBtn';
+import { useLogout } from '../../../auth/hooks/useLogout';
+import { useNavigate } from 'react-router-dom';
 
 export const MobileMenu = () => {
   const [isSearchOpened, setIsSearchOpened] = useState(false);
   const isAuth = useAuthStore((state) => state.isAuth);
-  const { logout } = useAuth();
+  const { mutate: logout } = useLogout();
+  const navigate = useNavigate();
   return (
     <div data-testid="mobile-menu" className={styles.menu}>
       {isSearchOpened && (
@@ -26,10 +29,13 @@ export const MobileMenu = () => {
           setIsSearchOpened(true);
         }}
       >
-        <MdSearch className={styles.icon} />
+        <MdSearch className={styles.search} />
       </button>{' '}
-      <HeartBtn className={styles.icon} />
-      <NavDropdown menuItems={getMenuItems(isAuth, logout.mutateAsync)} />
+      <HeartBtn
+        className={styles.icon}
+        onClick={() => navigate('/favorites')}
+      />
+      <NavDropdown menuItems={getMenuItems(isAuth, logout)} />
     </div>
   );
 };
