f6f2326c484c5272acc36751fb072de5732ad935
Alona Rahilevych <ragilevic@gmail.com>
2026-01-12 21:03:17 +0100

    feat:create OAuthSuccess page for redirecting after log in

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -9,6 +9,7 @@ import { EventsPage } from '../../pages/EventsPage';
 import { EventPage } from '../../pages/EventPage';
 import { FavoritesPage } from '../../pages/FavoritesPage';
 import { AllSearchResultsPage } from '../../pages/AllSearchResultsPage';
+import { OAuthSuccess } from '../../pages/OAuthSuccess';
 
 export const AppRoutes = () => {
   return (
@@ -18,6 +19,7 @@ export const AppRoutes = () => {
       <Route path="registration" element={<RegistrationPage />}></Route>
       <Route path="search" element={<AllSearchResultsPage />}></Route>
       <Route path="events/:categoryId" element={<EventsPage />}></Route>
+      <Route path="oauth/success" element={<OAuthSuccess />}></Route>
 
       <Route path="event/:id" element={<EventPage />}></Route>
       <Route
---
+++ src/pages/OAuthSuccess.tsx
@@ -0,0 +1,36 @@
+import { Footer } from 'react-day-picker';
+import { Header } from '../layouts/header/Header';
+import styles from '../features/favorites/styles/FavoritesPage.module.css';
+import { Loader } from '../shared/ui/loader/Loader';
+import { useEffect } from 'react';
+import { refreshAccessToken } from '../shared/lib/refreshAccessToken';
+import { useAuthStore } from '../features/auth/model/AuthStore';
+import toast from 'react-hot-toast';
+
+export const OAuthSuccess = () => {
+  const setIsAuth = useAuthStore((state) => state.setIsAuth);
+  useEffect(() => {
+    const getToken = async () => {
+      const token = await refreshAccessToken();
+      if (token) {
+        toast.success('Successfully logged in!');
+        setIsAuth(true);
+        window.location.replace('/');
+      } else {
+        window.location.replace('/login');
+      }
+    };
+    getToken();
+  }, []);
+  return (
+    <div className="wrapper">
+      <Header />
+      <main className={styles.main}>
+        <div className="container">
+          <Loader />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
